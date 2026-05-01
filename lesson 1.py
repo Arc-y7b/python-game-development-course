6ca7d566d6c84a38be87bdb0f05e5e37b29ef903
Arc-y7b <thisisaarav006@gmail.com>
2026-05-01 17:57:58 +0100

    Add files via upload

---
+++ lesson 1.py
@@ -0,0 +1,9 @@
+countrydb={}
+while True:
+    print("1. insert")
+    print("2. Display all countrys")
+    print("3. Display all capitals") 
+    print("4. Get capitals")
+    print("5. Delete")
+
+    choice = int
