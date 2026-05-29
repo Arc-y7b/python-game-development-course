8321ee2c4a39576f9c4f62da7b44c17f9096968d
Arc-y7b <thisisaarav006@gmail.com>
2026-05-29 17:53:42 +0100

    Add files via upload

--- lesson 1.py
+++ lesson 1.py
@@ -8,7 +8,40 @@ while True:
 
     choice = int(input("Enter your choice (1-5): "))
 
-if choice == 1:
-    country = input("enter country : ").upper()
-     capital = input("enter capital : ").upper()
-    countryDb[country] = capital
+    if choice == 1:
+      country = input("enter country : ").upper()
+      capital = input("enter capital : ").upper()
+      countrydb[country] = capital
+      print("Country and capital added successfully")
+
+    elif choice == 2:
+      print("\nCountries:")
+      for country in countrydb:
+       print(country)
+
+    elif choice == 3:
+      print("\nCapital:")
+      for capital in countrydb.values():
+       print(capital)
+
+    elif choice == 4:
+     country = input ("Enter country: ").upper()
+
+     if country in countrydb:
+       print("Capital:",countrydb[country])
+     else:
+       print("country not found.")
+
+    elif choice ==5:
+      country = input("Enter country to delete:").upper()
+
+      if country in countrydb:
+        del countrydb[country]
+        print("Country deleted successfully.")
+      else:
+       print("Country not found.")
+    elif choice == 6:
+      print("Exiting program...")
+
+    else:
+      print("Invalid choice.Please try again.")
